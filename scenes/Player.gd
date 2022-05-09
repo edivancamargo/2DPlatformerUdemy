0e3a0bbd9df31090b99766d7cd40175eb72a84b4
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-10 00:02:31 +0100

    Improve player movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,25 +2,30 @@ extends KinematicBody2D
 
 var gravity: int = 1000
 var velocity: Vector2 = Vector2.ZERO
-var maxHorizontalSpeed: int = 125
+var maxHorizontalSpeed: int = 140
 var horizontalAcceleration: int = 2000
-var jumpSpeed: int = 320
+var jumpSpeed: int = 360
+var jumpTerminationMultiplier: int = 4
 var movePlayerVector: Vector2 = Vector2.ZERO
 
 func _process(delta) -> void:
 	velocity.x += movePlayerVector.x * horizontalAcceleration * delta
-	
+
 	if movePlayerVector.x == 0:
 		# https://www.gamedeveloper.com/programming/improved-lerp-smoothing-
 		# lerp is frame rate dependent. Doing the way below it fixes it.
 		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
-	
+
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
-	
-	if (movePlayerVector.y < 0 and is_on_floor()):
+
+	if (movePlayerVector.y < 0 && is_on_floor()):
 		velocity.y = movePlayerVector.y * jumpSpeed
-	
-	velocity.y += gravity * delta
+
+	if (velocity.y < 0 and not Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta	
+	else:
+		velocity.y += gravity * delta
+
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _input(event):
