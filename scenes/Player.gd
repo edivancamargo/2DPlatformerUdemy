236028362e6e347fe7911e4e98c14c3a5c8bba5d
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-09 23:03:09 +0100

    using lerp to decrease player horizontal velocity. Improved player movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,13 +1,21 @@
 extends KinematicBody2D
 
-var gravity: int = 400
+var gravity: int = 1000
 var velocity: Vector2 = Vector2.ZERO
-var maxHorizontalSpeed: int = 100
-var jumpSpeed: int = 180
+var maxHorizontalSpeed: int = 125
+var horizontalAcceleration: int = 2000
+var jumpSpeed: int = 320
 var movePlayerVector: Vector2 = Vector2.ZERO
 
 func _process(delta) -> void:
-	velocity.x = movePlayerVector.x * maxHorizontalSpeed
+	velocity.x += movePlayerVector.x * horizontalAcceleration * delta
+	
+	if movePlayerVector.x == 0:
+		# https://www.gamedeveloper.com/programming/improved-lerp-smoothing-
+		# lerp is frame rate dependent. Doing the way below it fixes it.
+		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+	
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
 	if (movePlayerVector.y < 0 and is_on_floor()):
 		velocity.y = movePlayerVector.y * jumpSpeed
