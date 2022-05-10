07331d4f69ebd902d7a88d25f4256642ba799e33
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-10 21:59:08 +0100

    Fix gamecamera Pixel Jittering

--- scenes/GameCamera/GameCamera.gd
+++ scenes/GameCamera/GameCamera.gd
@@ -3,19 +3,20 @@ extends Camera2D
 export(Color, RGB) var backgroundColor
 
 onready var players = get_tree().get_nodes_in_group("Player")
-var player = null
-var targetPosition := Vector2.ZERO
+
+var player: KinematicBody2D = null
+var cameraPositionX: int
+var cameraPositionY: int
 
 func _ready() -> void:
+	set_player()
 	VisualServer.set_default_clear_color(backgroundColor)
 
 func _process(delta) -> void:
-	get_target_position()
-	global_position = lerp(targetPosition, global_position, pow(2, -50 * delta))
+	cameraPositionX = int(lerp(player.global_position.x, global_position.x, pow(2, -15 * delta)))
+	cameraPositionY = int(lerp(player.global_position.y, global_position.y, pow(2, -15 * delta)))
+	global_position = Vector2(cameraPositionX, cameraPositionY)
 
-func get_target_position() -> Vector2:
+func set_player() -> void:
 	if players.size() > 0:
-		player = players[0]
-		targetPosition = player.global_position
-	
-	return targetPosition
+		player = players[0] as KinematicBody2D
