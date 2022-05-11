f1012ee6aee618a3dc9c02785fd08705c17af9cc
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-11 08:29:33 +0100

    Add double jump

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -10,8 +10,12 @@ var horizontalAcceleration: int = 2000
 var jumpSpeed: int = 360
 var jumpTerminationMultiplier: int = 4
 var movePlayerVector: Vector2 = Vector2.ZERO
+var hasDoubleJump: bool = false
 
 func _process(delta) -> void:
+	movePlayerVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	movePlayerVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	
 	velocity.x += movePlayerVector.x * horizontalAcceleration * delta
 
 	if movePlayerVector.x == 0:
@@ -20,9 +24,13 @@ func _process(delta) -> void:
 		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
 
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
-
-	if (movePlayerVector.y < 0 && (is_on_floor() || !coyoteTimer.is_stopped())):
+	if (movePlayerVector.y < 0 && (is_on_floor() || !coyoteTimer.is_stopped() || hasDoubleJump)):
 		velocity.y = movePlayerVector.y * jumpSpeed
+		
+		if !is_on_floor() && coyoteTimer.is_stopped():
+			hasDoubleJump = false
+			
+		coyoteTimer.stop()
 
 	if (velocity.y < 0 and not Input.is_action_pressed("jump")):
 		velocity.y += gravity * jumpTerminationMultiplier * delta	
@@ -35,11 +43,10 @@ func _process(delta) -> void:
 	if wasOnFloor && !is_on_floor():
 		coyoteTimer.start()
 	
+	if is_on_floor():
+		hasDoubleJump = true
+	
 	update_animation()
-
-func _input(event):
-	movePlayerVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	movePlayerVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 
 func update_animation() -> void:
 	if not is_on_floor():
