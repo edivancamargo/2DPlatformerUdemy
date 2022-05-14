f4b77ab0cf1058747b60adfcb0270b524f0b2c30
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-14 22:16:58 +0100

    Extra base level from main scene. Runs BaseLevel scene by default

---
+++ scenes/Levels/BaseLevel.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var playerScene = preload("res://scenes/Player/Player.tscn")
+var spawnPosition = Vector2.ZERO
+onready var currentPlayer: KinematicBody2D = $Player
+var currentPlayerNode: KinematicBody2D = null
+
+func _ready() -> void:
+	spawnPosition = currentPlayer.global_position
+	SignalServiceManager.connect("player_hurt", self, "on_player_hurt", [], CONNECT_DEFERRED)
+	register_player(currentPlayer)
+	
+func register_player(player: KinematicBody2D) -> void:
+	currentPlayerNode = player
+
+func create_player() -> void:
+	var playerInstance = playerScene.instance()
+	add_child_below_node(currentPlayerNode, playerInstance)
+	playerInstance.global_position = spawnPosition
+	register_player(playerInstance)
+
+func on_player_hurt(playerSignal) -> void:
+	currentPlayerNode.queue_free()
+	create_player()
