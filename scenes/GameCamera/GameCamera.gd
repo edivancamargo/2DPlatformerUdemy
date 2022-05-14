76e6f98ced9a8653ae57268ef1eccf8a425616ec
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-14 22:46:13 +0100

    Fix camera issue when trying to return to spawn position after player die.

--- scenes/GameCamera/GameCamera.gd
+++ scenes/GameCamera/GameCamera.gd
@@ -1,22 +1,23 @@
 extends Camera2D
 
 export(Color, RGB) var backgroundColor
-
-onready var players = get_tree().get_nodes_in_group("Player")
-
-var player: KinematicBody2D = null
+var player: KinematicBody2D
+var targetPosition = Vector2.ZERO
 var cameraPositionX: int
 var cameraPositionY: int
 
 func _ready() -> void:
-	set_player()
 	VisualServer.set_default_clear_color(backgroundColor)
 
 func _process(delta) -> void:
+#	This is not right, we shouldn't be calling get_tree().get_nodes_in_group for every frame...
+	acquire_target_position()
 	cameraPositionX = int(lerp(player.global_position.x, global_position.x, pow(2, -15 * delta)))
 	cameraPositionY = int(lerp(player.global_position.y, global_position.y, pow(2, -15 * delta)))
 	global_position = Vector2(cameraPositionX, cameraPositionY)
 
-func set_player() -> void:
-	if players.size() > 0:
-		player = players[0] as KinematicBody2D
+func acquire_target_position() -> void:
+	var players = get_tree().get_nodes_in_group("Player")
+	if (players.size() > 0):
+		player = players[0]
+		targetPosition = player.global_position
