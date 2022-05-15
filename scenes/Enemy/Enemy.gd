2b9efbbd06c9f52d652560b454d5f08191c46200
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-15 23:04:26 +0100

    Add enemy movement and collision detection

--- scenes/Enemy/Enemy.gd
+++ scenes/Enemy/Enemy.gd
@@ -1,16 +1,24 @@
 extends KinematicBody2D
 
+enum Direction {RIGHT, LEFT}
+export(Direction) var initialDirection
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var maxSpeed: int = 25
+var velocity: Vector2 = Vector2.ZERO
+var direction: Vector2 = Vector2.RIGHT
+var gravity: int = 500
+onready var animatedSprite: AnimatedSprite = $AnimatedSprite
 
+func _ready() -> void:
+	direction = Vector2.RIGHT if initialDirection == Direction.RIGHT else Vector2.LEFT
+	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta) -> void:
+	velocity.x = (direction * maxSpeed).x
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	animatedSprite.flip_h = true if direction.x > 0 else false
+	
+func on_goal_entered(_area2D: Area2D) -> void:
+	direction *= -1
