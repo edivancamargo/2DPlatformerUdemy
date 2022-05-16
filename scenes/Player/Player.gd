a51985a5db808a65ea26810fc6931441dbb1603b
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-16 08:23:20 +0100

    Add state machine to the player and new dashing state.

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -4,9 +4,15 @@ onready var animatedSprite: AnimatedSprite = $AnimatedSprite
 onready var coyoteTimer: Timer = $CoyoteTimer
 onready var hurtboxArea: Area2D = $HurtboxArea
 
+enum State { NORMAL, DASHING }
+var currentState = State.NORMAL
+var isStateNew = true
+
 var gravity: int = 1000
 var velocity: Vector2 = Vector2.ZERO
 var maxHorizontalSpeed: int = 140
+var maxDashSpeed: int = 500
+var minDashSpeed: int = 200
 var horizontalAcceleration: int = 2000
 var jumpSpeed: int = 360
 var jumpTerminationMultiplier: int = 4
@@ -17,8 +23,26 @@ func _ready() -> void:
 	hurtboxArea.connect("area_entered", self, "on_hurtbox_area_entered")
 
 func _process(delta) -> void:
-	movePlayerVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	movePlayerVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	match currentState:
+		State.NORMAL:
+			handle_normal_state(delta)
+		State.DASHING:
+			handle_dash_state(delta)
+			
+	isStateNew = false
+
+func change_state(new_state) -> void:
+	currentState = new_state
+	isStateNew = true
+
+func get_movement_vector() -> Vector2:
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return moveVector
+
+func handle_normal_state(delta) -> void:
+	movePlayerVector = get_movement_vector()
 	
 	velocity.x += movePlayerVector.x * horizontalAcceleration * delta
 
@@ -50,7 +74,29 @@ func _process(delta) -> void:
 	if is_on_floor():
 		hasDoubleJump = true
 	
+	if Input.is_action_just_pressed("dash"):
+		call_deferred("change_state", State.DASHING)
+	
 	update_animation()
+
+func handle_dash_state(delta) -> void:
+	if isStateNew:
+		animatedSprite.play("Jump")
+		var move_vector = get_movement_vector()
+		var velocityMod = 1
+		
+		if move_vector.x != 0:
+			velocityMod = sign(move_vector.x)
+		else:
+			velocityMod = 1 if animatedSprite.flip_h else -1
+			
+		velocity = Vector2(maxDashSpeed * velocityMod, 0)
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(0, velocity.x, pow(2, -5 * delta))
+	
+	if abs(velocity.x) < minDashSpeed:
+		call_deferred("change_state", State.NORMAL)
 
 func update_animation() -> void:
 	if not is_on_floor():
