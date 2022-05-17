2c8650d145d2f58d1d79e2f5df7811691df79a78
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-17 08:23:29 +0100

    When hurtbox area entered destroy enemy.

--- scenes/Enemy/Enemy.gd
+++ scenes/Enemy/Enemy.gd
@@ -12,6 +12,7 @@ onready var animatedSprite: AnimatedSprite = $AnimatedSprite
 func _ready() -> void:
 	direction = Vector2.RIGHT if initialDirection == Direction.RIGHT else Vector2.LEFT
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
+	$HurtBox.connect("area_entered", self, "on_hurtbox_entered")
 
 func _process(delta) -> void:
 	velocity.x = (direction * maxSpeed).x
@@ -22,3 +23,6 @@ func _process(delta) -> void:
 	
 func on_goal_entered(_area2D: Area2D) -> void:
 	direction *= -1
+
+func on_hurtbox_entered(_area2D: Area2D) -> void:
+	queue_free()
