043c4a47dd0999e0544a5bd7ca7dd13d27332d95
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-18 07:24:16 +0100

    Create Flag scene to finish the level

---
+++ src/scenes/Flag/Flag.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+onready var collisionArea = $Area2D
+
+func _ready() -> void:
+	collisionArea.connect("area_entered", self, "on_area_entered")
+
+func on_area_entered(_area: Area2D) -> void:
+	SignalServiceManager.emit_level_cleared()
--- src/services/SignalServiceManager.gd
+++ src/services/SignalServiceManager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 signal player_hurt
+signal level_cleared
 
 func emit_player_hurt(player: KinematicBody2D) -> void:
 	emit_signal("player_hurt", player)
+
+func emit_level_cleared() -> void:
+	emit_signal("level_cleared")
