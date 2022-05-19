98c56fe171604dfda8232912f0bb098ff10f96d2
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-19 21:46:25 +0100

    Improve UI coins display. Add animated texture.

--- src/helpers/Levels/LevelUI/LevelUI.gd
+++ src/helpers/Levels/LevelUI/LevelUI.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-onready var coinsDisplayLbl: Label = $MarginContainer/CoinsDisplayLbl
+onready var coinsDisplayLbl: Label = $MarginContainer/HBoxContainer/CoinsDisplayLbl
 
 func _ready() -> void:
 	SignalServiceManager.connect("total_coins_changed", self, "on_total_coins_changed")
