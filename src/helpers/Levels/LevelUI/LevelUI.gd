33b2be9ae73c0847e2348e2c8c70e5b2ff63ba2f
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-19 08:56:56 +0100

    Update UI to show the number of coins collected.

---
+++ src/helpers/Levels/LevelUI/LevelUI.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+onready var coinsDisplayLbl: Label = $MarginContainer/CoinsDisplayLbl
+
+func _ready() -> void:
+	SignalServiceManager.connect("total_coins_changed", self, "on_total_coins_changed")
+
+func on_total_coins_changed(totalCoins: int, collectedCoins: int) -> void:
+	coinsDisplayLbl.text = str(collectedCoins,"/", totalCoins)
