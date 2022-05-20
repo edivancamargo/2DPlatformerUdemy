ed13da6cad9222919d1eefd5992de921126ae837
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-20 08:30:50 +0100

    Add enemy initial direction to the enemy spawner

--- src/scenes/EnemySpawner/EnemySpawner.gd
+++ src/scenes/EnemySpawner/EnemySpawner.gd
@@ -1,1 +1,30 @@
 extends Position2D
+
+enum Direction {RIGHT, LEFT}
+
+export(PackedScene) var enemyScene
+export(Direction) var startDirection
+
+var currentEnemyNode = null
+var spawnOnNextTick = false
+
+func _ready() -> void:
+	$Timer.connect("timeout", self, "on_spawn_timer_timeout")
+	call_deferred("spawn_enemy")
+	
+func spawn_enemy() -> void:
+	currentEnemyNode = enemyScene.instance()
+	currentEnemyNode.startDirection = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
+	currentEnemyNode.global_position = global_position
+	get_parent().add_child(currentEnemyNode)
+
+func on_spawn_timer_timeout() -> void:
+	check_enemy_spawn()
+
+func check_enemy_spawn() -> void:
+	if !is_instance_valid(currentEnemyNode):
+		if spawnOnNextTick:
+			spawn_enemy()
+			spawnOnNextTick = false
+		else:
+			spawnOnNextTick = true
