8a51a030816cb618384e0a80f440efd858341844
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-20 08:32:40 +0100

    Allow the enemy direction to be overridden by the diretion in enemy spawner

--- src/scenes/Enemy/Enemy.gd
+++ src/scenes/Enemy/Enemy.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 enum Direction {RIGHT, LEFT}
 export(Direction) var initialDirection
 
+var startDirection: Vector2 = Vector2.ZERO
 var maxSpeed: int = 25
 var velocity: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.RIGHT
@@ -10,7 +11,7 @@ var gravity: int = 500
 onready var animatedSprite: AnimatedSprite = $AnimatedSprite
 
 func _ready() -> void:
-	direction = Vector2.RIGHT if initialDirection == Direction.RIGHT else Vector2.LEFT
+	set_enemy_direction()
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 	$HurtBox.connect("area_entered", self, "on_hurtbox_entered")
 
@@ -20,7 +21,13 @@ func _process(delta) -> void:
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	animatedSprite.flip_h = true if direction.x > 0 else false
-	
+
+func set_enemy_direction() -> void:
+	if startDirection != Vector2.ZERO:
+		direction = startDirection
+	else:
+		direction = Vector2.RIGHT if initialDirection == Direction.RIGHT else Vector2.LEFT
+
 func on_goal_entered(_area2D: Area2D) -> void:
 	direction *= -1
 
