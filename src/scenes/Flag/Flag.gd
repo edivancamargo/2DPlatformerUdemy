53879a7b6d0499aaa3f2cd58cf9e19059bf70e45
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-22 13:09:08 +0100

    Update Flag to emit correct signal.

--- src/scenes/Flag/Flag.gd
+++ src/scenes/Flag/Flag.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	collisionArea.connect("area_entered", self, "on_area_entered")
 
 func on_area_entered(_area: Area2D) -> void:
-	SignalServiceManager.emit_level_cleared()
+	SignalServiceManager.emit_level_complete()
