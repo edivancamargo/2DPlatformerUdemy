f60067dc791b33c81ddcae2d7a6ea27011805b4c
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-26 22:03:31 +0100

    Add a small effect when player dies. Fix issue related to the position of the sprite when player dies

--- src/scenes/Player/PlayerDeath.gd
+++ src/scenes/Player/PlayerDeath.gd
@@ -1,7 +1,14 @@
 extends KinematicBody2D
 
+onready var visuals: Node2D = $Visuals
+
 var velocity = Vector2.ZERO
 var gravity:int = 1000
+
+func _ready() -> void:
+	if velocity.x > 0:
+		print("inside")
+		visuals.scale = Vector2(-1, 1)
 
 func _process(delta) -> void:
 	velocity.y += gravity * delta
