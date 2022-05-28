ccc2131ffe8fd20a00212d4be35a852c5ed26703
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-28 21:54:14 +0100

    Finish Enemy Spawn animation

--- src/scenes/Enemy/Enemy.gd
+++ src/scenes/Enemy/Enemy.gd
@@ -4,13 +4,15 @@ var enemyDeathScene = preload("res://src/scenes/Enemy/EnemyDeath.tscn")
 
 enum Direction {RIGHT, LEFT}
 export(Direction) var initialDirection
+export var isSpawning: bool = false
+
+onready var animatedSprite: AnimatedSprite = $Visuals/AnimatedSprite
 
 var startDirection: Vector2 = Vector2.ZERO
 var maxSpeed: int = 25
 var velocity: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.RIGHT
 var gravity: int = 500
-onready var animatedSprite: AnimatedSprite = $AnimatedSprite
 
 func _ready() -> void:
 	set_enemy_direction()
@@ -18,6 +20,9 @@ func _ready() -> void:
 	$HurtBox.connect("area_entered", self, "on_hurtbox_entered")
 
 func _process(delta) -> void:
+	if isSpawning:
+		return
+	
 	velocity.x = (direction * maxSpeed).x
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
